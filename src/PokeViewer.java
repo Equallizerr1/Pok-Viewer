1c686c671f5e0369bb146d3d5efb3bfb75d10aff
Equallizerr1 <samharradine1994@gmail.com>
2024-06-13 22:52:18 +0100

    Created PokéViewer app with a basic JFrame gui and an API call to the PokéAPI to gather pokémon data.

---
+++ src/PokeViewer.java
@@ -0,0 +1,96 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+import java.util.HashMap;
+
+public class PokeViewer implements  ActionListener{
+    JFrame frame;
+    JTextField textField;
+    JButton searchButton;
+    JPanel panel;
+
+    Font myFont = new Font("Arial", Font.BOLD, 30);
+
+    String searchTerm;
+    HashMap<String, String> pokemonData = new HashMap<String, String>();
+
+    PokeViewer () {
+        MyCanvas m = new MyCanvas();
+
+        frame = new JFrame("PokéViewer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(100,25,300,50);
+        textField.setEditable(true);
+        textField.setFont(myFont);
+
+        searchButton = new JButton("Search");
+        searchButton.setBounds(400,25,100,50);
+        searchButton.addActionListener(this);
+
+        panel = new JPanel();
+        panel.setBounds(100, 100,400,400);
+        panel.setBackground(Color.lightGray);
+
+        frame.add(m);
+        frame.add(panel);
+        frame.add(searchButton);
+        frame.add(textField);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        PokeViewer poke = new PokeViewer();
+    }
+    public Object getPokemon(String searchTerm) {
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://pokeapi.co/api/v2/pokemon/"+searchTerm))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+        assert response != null;
+        JsonObject pokemonObject = new Gson().fromJson(response.body(), JsonObject.class);
+        //System.out.println(pokemonObject.getAsJsonObject().get("sprites").getAsJsonObject().get("front_default"));
+        pokemonData.put("Name", String.valueOf(pokemonObject.get("name")));
+        pokemonData.put("Sprite", String.valueOf(pokemonObject.getAsJsonObject().get("sprites").getAsJsonObject().get("front_default")));
+        return pokemonData;
+    }
+
+    public class MyCanvas extends Canvas {
+        public void paint(Graphics g) {
+            Toolkit t = Toolkit.getDefaultToolkit();
+            Image i = t.getImage(pokemonData.get("Sprites"));
+            g.drawImage(i, 120,100,this);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == searchButton) {
+        searchTerm = textField.getText();
+        getPokemon(searchTerm);
+
+        System.out.println(pokemonData.get("Sprite"));
+        }
+
+    }
+}
