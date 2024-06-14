4fffd2479575e7b731bd2f778f5a2825d6706818
Equallizerr1 <samharradine1994@gmail.com>
2024-06-14 11:23:54 +0100

    Removed code that it now is separate files/Classes

--- src/PokeViewer.java
+++ src/PokeViewer.java
@@ -1,15 +1,11 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import java.net.URL;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpClient;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -28,7 +24,6 @@ public class PokeViewer implements  ActionListener{
     static HashMap<String, String> pokemonData = new HashMap<String, String>();
 
     PokeViewer () {
-        MyCanvas m = new MyCanvas();
 
         frame = new JFrame("PokéViewer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,7 +43,6 @@ public class PokeViewer implements  ActionListener{
         panel.setBounds(100, 100,400,400);
         panel.setBackground(Color.lightGray);
 
-        frame.add(m);
         frame.add(panel);
         frame.add(searchButton);
         frame.add(textField);
@@ -56,14 +50,9 @@ public class PokeViewer implements  ActionListener{
     }
 
     public static void main(String[] args) throws IOException {
-        String imageUrl = pokemonData.get("sprite");
-        String destinationFile = "image.png";
-
-        saveImage(imageUrl, destinationFile);
         PokeViewer poke = new PokeViewer();
-
     }
-    public Object getPokemon(String searchTerm) {
+    public void getPokemon(String searchTerm) {
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://pokeapi.co/api/v2/pokemon/"+searchTerm))
@@ -81,31 +70,6 @@ public class PokeViewer implements  ActionListener{
         //System.out.println(pokemonObject.getAsJsonObject().get("sprites").getAsJsonObject().get("front_default"));
         pokemonData.put("Name", String.valueOf(pokemonObject.get("name")));
         pokemonData.put("Sprite", String.valueOf(pokemonObject.getAsJsonObject().get("sprites").getAsJsonObject().get("front_default")));
-        return pokemonData;
-    }
-
-    public static void saveImage(String imageUrl, String destinationFile) throws  IOException{
-        URL url = new URL(imageUrl);
-        InputStream is = url.openStream();
-        OutputStream os = new FileOutputStream(destinationFile);
-
-        byte[] b = new byte[2048];
-        int length;
-
-        while ((length = is.read(b)) != -1) {
-            os.write(b,0,length);
-        }
-
-        is.close();
-        os.close();
-    }
-
-    public static class MyCanvas extends Canvas {
-        public void paint(Graphics g) {
-            Toolkit t = Toolkit.getDefaultToolkit();
-            Image i = t.getImage("image");
-            g.drawImage(i, 120,100,this);
-        }
     }
 
     @Override
@@ -114,8 +78,7 @@ public class PokeViewer implements  ActionListener{
         searchTerm = textField.getText();
         getPokemon(searchTerm);
 
-        System.out.println(pokemonData.get("Sprite"));
+        //System.out.println(pokemonData.get("Sprite"));
         }
-
     }
 }
